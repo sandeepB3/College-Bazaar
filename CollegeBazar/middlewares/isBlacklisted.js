fc495b8ed14384a95e6486d974edd7d8dd234ad9
sandeep <107111616+sandeepB3@users.noreply.github.com>
2022-10-21 22:09:32 +0530

    Adding new Directory

--- CollegeBazar/middlewares/isBlacklisted.js
+++
@@ -1,8 +0,0 @@
-const isBlacklisted=(req,res,next)=>{
-if(req.user.status==="Blacklisted"){
-    req.flash('error','You have been blacklisted by the admin!')
-    return res.redirect('/')
-}
-next()
-}
-module.exports=isBlacklisted
